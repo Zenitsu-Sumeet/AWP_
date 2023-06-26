2580ef137a115a68625ff3e51373c394b3d61ce5
Zenitsu-Sumeet <thesumeetpay@gmail.com>
2023-06-26 15:23:22 +0530

    ADDED THE FILES

--- progams/array.cs
+++ progams/array.cs
@@ -4,11 +4,16 @@ namespace SumeetApp26jun2023
 {
     class array
     {
-        static void Main(string[] args)
+         static void Main(string[] args)
         {
             ArrayList a = new ArrayList();
             a.Add(12);
-
+            a.Add(155);
+            a.Add("sumeet");
+            foreach(object i in a)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
---
+++ progams/details.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SumeetApp26jun2023.progams
+{
+    class details
+    {
+        static void print(String[] arrgs)
+        {
+            String name = "sumeet vishwakarma";
+
+        }
+    }
+}
