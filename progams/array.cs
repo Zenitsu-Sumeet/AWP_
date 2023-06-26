74a31ec99b85ac491291560f5ba2e5cca25a8e3e
Zenitsu-Sumeet <thesumeetpay@gmail.com>
2023-06-26 15:03:51 +0530

    added

---
+++ progams/array.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+namespace SumeetApp26jun2023
+{
+    class array
+    {
+        static void Main(string[] args)
+        {
+            ArrayList a = new ArrayList();
+            a.Add(12);
+
+        }
+    }
+}
