7aa0a17dc94f435e0f61d93f38df74dd1ffcb555
SCMS <SCMS@PC-21>
2023-06-26 14:53:55 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SumeetApp26jun2023
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] number = { 1, 2, 5, 4, 5 };
+            foreach(int r in number)
+            {
+                Console.WriteLine(r);
+            }
+
+        }
+    }
+}
