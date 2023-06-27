8f6dcc5e8ecef48b07e7ba6a2863b292588d1c1c
Zenitsu-Sumeet <thesumeetpay@gmail.com>
2023-06-27 13:56:42 +0530

    adde

--- progams/Program.cs
+++ progams/Program.cs
@@ -9,14 +9,33 @@ namespace SumeetApp26jun2023
             {   Console.WriteLine(r);
             }
             string N = "Sumeet vishwakarma";
+            string me = " coder";
+            Console.WriteLine("--------------------");
             string u = N.Insert(2, "sum");
             Console.WriteLine(u);
+            Console.WriteLine("--------------------");
             string z = N.Remove(6, 7);
             Console.WriteLine(z);
+            Console.WriteLine("--------------------");
             int i = N.IndexOf("t");
             Console.WriteLine(i);
+            Console.WriteLine("--------------------");
             int l = N.LastIndexOf("a");
             Console.WriteLine(l);
+            Console.WriteLine("--------------------");
+            string c = String.Concat(N, me);
+            Console.WriteLine(c);
+            Console.WriteLine("--------------------");
+            string a = "coder";
+            string b = "coder";
+            bool answer = string.Equals(a, b);
+            Console.WriteLine(answer);
+            Console.WriteLine("--------------------");
+            String he = "hello World";
+            bool t = he.Contains("wo");
+            Console.WriteLine(t);
+            Console.WriteLine("--------------------");
+
         }
     }
 }
